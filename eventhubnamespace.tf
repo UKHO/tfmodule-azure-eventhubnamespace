1b006cae1529ac5f520e2cabaef0693ed51421fa
Luke Duddridge <205003513+ld-at-work@users.noreply.github.com>
2025-07-24 16:07:00 +0100

    This is the right url now

--- eventhubnamespace.tf
+++ eventhubnamespace.tf
@@ -10,7 +10,7 @@ resource "azurerm_eventhub_namespace" "ehns" {
 
 module "eventhubs" {
   count  = length(var.event_hubs)
-  source = "github.com/ukho/terraform-azure-event-hub?ref=v0.6.0"
+  source = "github.com/ukho/tfmodule-azure-event-hub?ref=v0.6.0"
   providers = {
     src = azurerm.src
   }
